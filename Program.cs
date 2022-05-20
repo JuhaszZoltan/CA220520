1f742919a0f0d40b324bde6cee9be9c59312f45d
JuhaszZoltan <JuhaszZoltanMate@yahoo.com>
2022-05-20 12:25:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+Random rnd = new();
+
+
+//egy soros komment
+
+//Console.Write("írd be a neved: ");
+//string nev = Console.ReadLine();
+//Console.Clear();
+//Console.WriteLine($"Szia {nev}!");
+
+Console.CursorVisible = false;
+
+int szelesseg = Console.WindowWidth;
+int magassag = Console.WindowHeight;
+
+for (int i = 0; i < 100; i++)
+{
+    int w = rnd.Next(szelesseg);
+    int h = rnd.Next(magassag);
+
+    Console.SetCursorPosition(w, h);
+    Console.ForegroundColor = (ConsoleColor)rnd.Next(16);
+    Console.WriteLine("Zoli");    
+}
+
+Console.ReadKey();
